f0e0ca70401a1b73128c57f8225ed8e60e2e975f
Alonso <abarquero@socialware.com>
2016-12-16 11:59:18 -0600

    updating service

--- service.py
+++ service.py
@@ -96,4 +96,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=11000)
--- test_code.py
+++ test_code.py
@@ -14,6 +14,7 @@ description = 'description of task {}'.format(task_name)
 # response = session.post("http://localhost:5000/todo/api/v1.0/tasks", headers=headers,
 #                         data=dumps({'title': task_name, 'description': description}))
 
-response = session.get('http://localhost:5000/todo/api/v1.0/tasks/-1')
-print response.status_code
+for x in xrange(5):
+    response = session.get('http://172.16.12.57:5000/todo/api/v1.0/html')
+    print response.status_code
 
